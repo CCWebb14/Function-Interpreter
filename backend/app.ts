170c0232cc5c4543abd0b379aa421c09c9c6dc10
UBC Student <jgan07@students.cs.ubc.ca>
2024-06-27 13:32:47 -0700

    CORS integration for security
    Ensures no malicious endpoint access utilizing cors.

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/userRoutes';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 4001;
 
-// Middleware to parse JSON bodies
+
+//Middlewares
+app.use(cors({
+    origin: 'http://localhost:4000' // Replace with your frontend's URL
+}));
+//Lack security: app.use(cors());
 app.use(bodyParser.json());
 
 // Use the user routes
