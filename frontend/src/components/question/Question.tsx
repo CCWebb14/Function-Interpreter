1d1b572b171751c0dfdf0e0cf5c80bfd92d61a8a
webb47 <webb47@students.cs.ubc.ca>
2024-07-06 17:07:08 -0700

    Question component now returns an alert based on llm results

--- frontend/src/components/question/Question.tsx
+++ frontend/src/components/question/Question.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import '../../styles/app.css'
 import '../../styles/question.css'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import TextField from '@mui/material/TextField';
 import { useParams } from 'react-router-dom';
 import { useQuestionFetch } from '../../hooks/useQuestionFetch';
@@ -11,29 +12,45 @@ import AlertTitle from '@mui/material/AlertTitle';
 
 const initialState = {
     success: '',
-    test_result: '',
+    tests_passed: '',
+    tests_failed: '',
     llm_function: '',
+    returned: false,
+    complete_success: false,
 };
 
 interface StateType {
     success: string;
-    test_result: string;
+    tests_passed: string;
+    tests_failed: string;
     llm_function: string;
+    returned: boolean;
+    complete_success: boolean;
+}
+
+const submitErrorInitialState = {
+    errorMsg: '',
+    error: false,
+}
+
+interface SubmitErrorStateType {
+    errorMsg: string;
+    error: boolean;
 }
 
 type response = {
     data: {
         success: string;
-        test_result: string;
+        tests_passed: string;
+        tests_failed: string;
         llm_function: string;
     }
 }
 
 export default function Question() {
     const [user_input, setUserInput] = useState('');
-    const [results, setResults] = useState(false);
-    const [state] = useState<StateType>(initialState);
-    const [submitError, setSubmitError] = useState('');
+    const [state, setState] = useState<StateType>(initialState);
+    const [submitError, setSubmitError] = useState<SubmitErrorStateType>(submitErrorInitialState);
     const { id } = useParams();
     const { questionFetchState, loading, error, setIsLoadingMore } =
     useQuestionFetch(id);
@@ -46,51 +63,98 @@ export default function Question() {
                 user_input
         });
             if (axios_response.data.success) {
-                console.log(axios_response.data.test_result)
-                setResults(true);
-                state.success = axios_response.data.success;
-                state.test_result = axios_response.data.test_result;
-                state.llm_function = axios_response.data.llm_function;
+                setState({
+                    success : axios_response.data.success,
+                    tests_passed : axios_response.data.tests_passed,
+                    tests_failed : axios_response.data.tests_failed,
+                    llm_function : axios_response.data.llm_function,
+                    returned : true,
+                    complete_success : (axios_response.data.tests_passed === axios_response.data.tests_failed)
+                });
+                setSubmitError(submitErrorInitialState);
             } else {
-                setSubmitError('Something went wrong');
+                setSubmitError({
+                    errorMsg : 'Server connection failed. Please try again.',
+                    error : true,
+                });
             }
         } catch (err) {
-            setSubmitError('An error occurred. Please try again.');
+            if (axios.isAxiosError(err)) {
+                setSubmitError({
+                    errorMsg : 'Make sure no Javascript syntax is present. Please rephrase and try again.',
+                    error : true,
+                });
+            } else {
+                setSubmitError({
+                    errorMsg : 'An unexpected error occured. Please try again.',
+                    error : true,
+                });
+            }
         }
 
         
     };
 
+    let alertContent;
+
+    if (submitError.error) {
+        alertContent =
+        (<Alert severity="error">
+            <AlertTitle>Warning</AlertTitle>
+                {submitError.errorMsg}
+        </Alert>)
+    } else if (state.complete_success) {
+        alertContent =
+        (<Alert severity="success">
+            <AlertTitle>Success</AlertTitle>
+                {`Tests passed: ${state.tests_passed}/${state.tests_failed}`}
+        </Alert>)
+    } else if (state.returned) {
+        alertContent = 
+        (<Alert severity="error" color='warning'>
+            <AlertTitle>Incorrect</AlertTitle>
+            {`Tests passed: ${state.tests_passed}/${state.tests_failed}`}
+        </Alert>)
+    } else {
+        alertContent = null;
+    }
+
     return(
-        <div className='box-container-q'>
-            <div className='box'>
-                <SyntaxHighlighter  language="javascript" showLineNumbers
-                customStyle={{display:'flex', width:'100%', flex: 1, padding: 0}} >
-                    {questionFetchState.results}
-                </SyntaxHighlighter>
-                <TextField
-                    id="standard-multiline-static"
-                    label="Interpretation"
-                    multiline
-                    rows={4}
-                    variant="filled"
-                    fullWidth
-                    required
-                    sx={{display : 'flex'}}
-                    onChange={
-                        (e) => setUserInput(e.target.value)
-                    }
-                />
-                <div className='footer'>
-                    <div className='spacer'></div>
-                    {results ? (
-                        <Alert severity="success">
-                        <AlertTitle>Success</AlertTitle>
-                            {state.test_result}
-                        </Alert>
-                    ) : null}
-                    
-                    <div onClick={handleSubmit} className="submit-button">Submit</div>
+        <div className='question-container'>
+            <div className='box-container-out'>
+                <div className='box'>
+                    <div className='question-header'>Output</div>
+                    <SyntaxHighlighter  language="javascript" showLineNumbers style={dark}
+                    customStyle={{display:'flex', width:'100%', flex: 1, padding: 0}} >
+                        {state.llm_function}
+                    </SyntaxHighlighter>
+                </div>
+            </div>
+            <div className='box-container-q'>
+                <div className='box'>
+                    <div className='question-header'>Question #{id}</div>
+                    <SyntaxHighlighter  language="javascript" showLineNumbers
+                    customStyle={{display:'flex', width:'100%', flex: 1, padding: 0}} >
+                        {questionFetchState.results}
+                    </SyntaxHighlighter>
+                    <TextField
+                        id="standard-multiline-static"
+                        label="Interpretation"
+                        multiline
+                        rows={4}
+                        variant="filled"
+                        fullWidth
+                        required
+                        sx={{display : 'flex'}}
+                        onChange={
+                            (e) => setUserInput(e.target.value)
+                        }
+                    />
+                    <div className='footer'>
+                        <div className='spacer'></div>
+                        {alertContent}
+                        <div onClick={handleSubmit} className="submit-button">Submit</div>
+                    </div>
                 </div>
             </div>
         </div>
