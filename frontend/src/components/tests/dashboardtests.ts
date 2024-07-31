d05f2e2838eed95152dff7e9f1fefe27edfd5a59
Andrew Park <andrewpark2396@gmail.com>
2024-07-31 10:43:37 -0700

    dashboard test update

--- frontend/src/components/tests/dashboardtests.ts
+++ frontend/src/components/tests/dashboardtests.ts
@@ -6,17 +6,16 @@ export function runDashboardTests() {
 
     const apiUrl = 'http://localhost:4001/api/users/';
 
-    describe('Dashboard API', function () {
-
-        it('')
-
-
-        it('should get user statistics', async function () {
+    describe('Dashboard Functionality API', function () {
+        
+        it('should contain user statistics', async function () {
                 const response = await axios.get(`${apiUrl}/dashboard`);
                 expect(response.status).to.equal(200);
                 expect(response.data).to.have.property('success', true);
-                expect(response.data).to.have.property('totalTime', 0);
-
+                expect(response.data).to.have.property('userName');
+                expect(response.data).to.have.property('totalTime');
+                expect(response.data).to.have.property('attemptedQuestions');
+                expect(response.data).to.have.property('passedQuestions');
         });
     });
 
